3273a9e9670f8f86a87ad93f8107c854bd367f08
BW <68707117+wjdanfu@users.noreply.github.com>
2021-01-15 16:45:18 +0900

    1/15 DP

---
+++ DP/이코테_개미_전사.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+array = list(map(int, input().split()))
+
+d= [0]*100
+
+d[0] = array[0]
+d[1] = max(array[0], array[1])
+for i in range(2, n):
+    d[i] = max(d[i - 2]+ array[i], d[i -1])
+
+print(d[n - 1])
+
+'''
+ex)
+9 1 2 4 5 2 4 8 6
+
+d
+9 9 11 13 16 16 20 24 26
+'''
