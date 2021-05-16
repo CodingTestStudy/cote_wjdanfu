03d44d414a76d971684b728def5800d60000b917
wjdanfu <68707117+wjdanfu@users.noreply.github.com>
2021-05-16 23:38:26 +0900

    5/16 수학

--- 수학/15762.py
+++ 수학/15762.py
@@ -1,5 +1,6 @@
 x= list(map(int,input().split()))
 
-x.sort(reverse=True)
-
-print(x[0]*x[1]//x[2])
+if x[0]*x[1]/x[2]>x[0]/x[1]*x[2]:
+  print(int(x[0]*x[1]/x[2]))
+else:
+  print(int(x[0]/x[1]*x[2]))
